74c13e91984319ac42321dbff98d4976ef87351a
Spencer Morris <swdmorris@gmail.com>
2017-07-25 21:10:51 -0700

    Add map components and GPS points

---
+++ MeadoWatch/MapController.swift
@@ -0,0 +1,15 @@
+//
+//  MapController.swift
+//  MeadoWatch
+//
+//  Created by Spencer Morris on 7/25/17.
+//  Copyright © 2017 Spencer Morris. All rights reserved.
+//
+
+import MapKit
+
+class MapController: UIViewController {
+    @IBOutlet private weak var mapView: MKMapView!
+    
+    
+}
